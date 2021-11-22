da72d9ca2877b364d85a718ae7c0c394171fb5e8
Adrian Czerwiński <adrczer@gmail.com>
2021-11-22 07:56:08 +0100

    BlaBla

--- app/src/main/java/com/adrianczerwinski/invoicemaker/MainActivity.kt
+++ app/src/main/java/com/adrianczerwinski/invoicemaker/MainActivity.kt
@@ -18,6 +18,7 @@ import com.adrianczerwinski.invoicemaker.data.models.Seller
 import com.adrianczerwinski.invoicemaker.data.viemodels.SellerViewModel
 import com.adrianczerwinski.invoicemaker.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -36,6 +37,8 @@ class MainActivity : AppCompatActivity() {
         var firstTime = preferences.getString("FirstTimeInstall", "")
         var name = preferences.getString("Name", "")
 
+        //Change for traingin puproses
+        //BlaBlaBla
 
 
         if (firstTime == "YES" && name != "") {
